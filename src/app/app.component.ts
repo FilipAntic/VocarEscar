db8d002265f7bccdbac9dad15e9a68a878df3718
FilipH <antic.filip996@gmail.com>
2017-10-07 15:15:30 +0200

    Menu i carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { LocaleService, Language, TranslationService } from 'angular-l10n';
+import { SelectItem } from 'primeng/primeng';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  @Language() lang: string;
+  currentSlide: number = 0;
+  languages: SelectItem[];
+  selectedLanguage: any;
+  constructor(public locale: LocaleService, private translation: TranslationService) { }
+
+  ngOnInit() {
+    this.languages = [];
+    let item: SelectItem = { label: this.translation.translate('menu.menuLanguageEnglish'), value: 1 };
+    this.languages.push(item);
+    item = { label: this.translation.translate('menu.menuLanguageSerbian'), value: 2 };
+    this.languages.push(item);
+    // item = { label: this.translation.translate('menu.menuLanguageFrench'), value: 3 };
+    // this.languages.push(item);
+    if (this.lang === 'en') {
+      this.selectedLanguage = 1;
+    } else if (this.lang === 'sr') {
+      this.selectedLanguage = 2;
+    } else if (this.lang === 'fr') {
+      // this.selectedLanguage = 3;
+    }
+    console.log(item);
+  }
+
+  onChange() {
+    if (this.selectedLanguage === 1) {
+      this.selectLocale('en', 'US', 'USD');
+    }
+    else if (this.selectedLanguage === 2) {
+      this.selectLocale('sr', 'RS', 'RSD');
+    }
+
+  }
+  selectLocale(language: string, country: string, currency: string): void {
+    this.locale.setDefaultLocale(language, country);
+    this.locale.setCurrentCurrency(currency);
+    location.reload();
+  }
+  selectLanguage(language: string): void {
+    this.locale.setCurrentLanguage(language);
+  }
+
+
+  onNext(): void {
+    if (this.currentSlide === 2) {
+      this.currentSlide = 0;
+      return;
+    }
+    this.currentSlide++;
+  }
+
+  onPrev(): void {
+    if (this.currentSlide === 0) {
+      this.currentSlide = 2;
+      return;
+    }
+    this.currentSlide--;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,97 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER, Injectable } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { TranslationModule, LocaleService, TranslationService } from 'angular-l10n';
+import { HttpModule } from '@angular/http';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductionComponent } from './production/production.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { DropdownModule } from 'primeng/primeng';
+import { FormsModule } from '@angular/forms';
+
+
+@Injectable()
+export class LocalizationConfig {
+
+  constructor(public locale: LocaleService, public translation: TranslationService) { }
+
+  load(): Promise<any> {
+
+    this.locale.addConfiguration()
+      .addLanguages(['en', 'sr'])
+      .setCookieExpiration(30)
+      .defineDefaultLocale('sr', 'RS').defineCurrency('RSD');
+    this.locale.init();
+
+    this.translation.addConfiguration()
+      .addProvider('./assets/locale/locale-');
+    this.translation.init();
+
+
+    const promise: Promise<any> = new Promise((resolve: any) => {
+      this.translation.translationChanged.subscribe(() => {
+        resolve(true);
+      });
+    });
+
+    this.translation.init();
+
+    return promise;
+  }
+
+}
+
+export function initLocalization(localizationConfig: LocalizationConfig): Function {
+  return () => localizationConfig.load();
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductsComponent,
+    ProductionComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpModule,
+    TranslationModule.forRoot(),
+    DropdownModule,
+    FormsModule
+
+
   ],
-  providers: [],
+  providers: [
+    LocalizationConfig,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initLocalization,
+      deps: [LocalizationConfig],
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(public locale: LocaleService, public translation: TranslationService) {
+
+    //   this.locale.addConfiguration()
+    //     .addLanguages(['en', 'sr'])
+    //     .setCookieExpiration(30)
+    //     .defineDefaultLocale('sr', 'RS')
+    //     .defineCurrency('RSD');
+
+    //   this.locale.init();
+
+    //   this.translation.addConfiguration()
+    //     .addProvider('./assets/locale/locale-');
+    //   this.translation.init();
+  }
+}
